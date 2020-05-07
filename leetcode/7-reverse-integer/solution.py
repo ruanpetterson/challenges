0843aedfa6c2b294920fa78ac76bbbebdd18885a
Ruan Petterson <ruan@petterson.eng.br>
2020-05-07 07:24:56 -0300

    [LeetCode] Reverse Integer

---
+++ leetcode/7-reverse-integer/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reverseString(self, s: str) -> str:
+        length = len(s)
+        result = [None] * length
+        start, end = 0, length - 1
+        while start <= end:
+            result[start], result[end] = s[end], s[start]
+            start, end = start + 1, end - 1
+        return ''.join(result)
+    
+    def reverse(self, x: int) -> int:
+        result = int(self.reverseString(str(abs(x))))
+        if result > 2147483647:
+            return 0
+        return result if x > 0 else (result * (-1))
