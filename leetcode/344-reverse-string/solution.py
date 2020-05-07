ffd77ebcbe018830ed244d145dd79f0245097b2f
Ruan Petterson <ruan@petterson.eng.br>
2020-05-07 07:28:43 -0300

    [LeetCode] Reverse String

--- leetcode/344-reverse-string/solution.py
+++ leetcode/344-reverse-string/solution.py
@@ -1,7 +1,6 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        size = len(s)
-        start, end = 0, size - 1
+        start, end = 0, len(s) - 1
         while (start <= end):
             s[start], s[end] = s[end], s[start]
             start, end = start + 1, end - 1
