c6ec1a189936293e0caec66949914b9bc0ec51dc
Ruan Petterson <ruan@petterson.eng.br>
2022-03-21 17:54:32 -0300

    [LeetCode] Reverse integer update
    Improved reverse algo and embedded into a trait

--- leetcode/7-reverse-integer/solution.rs
+++ leetcode/7-reverse-integer/solution.rs
@@ -1,10 +1,32 @@
-pub fn reverse_string(string: String) -> String {
-    return string.chars().rev().collect();
+pub trait Reverse {
+    fn rev(self) -> Self;
+    fn checked_rev(self) -> Option<Self> where Self: Sized;
+}
+
+impl Reverse for i32 {
+    fn rev(self) -> Self {
+        let mut input = self;
+        let mut rev = 0;
+        while input != 0 {
+            rev = rev * 10 + input % 10;
+            input /= 10;
+        }
+        rev
+    }
+    
+    fn checked_rev(self) -> Option<Self> {
+        let mut input = self;
+        let mut rev: Self = 0;
+        while input != 0 {
+            rev = rev.checked_mul(10)?.checked_add(input % 10)?;
+            input /= 10;
+        }
+        Some(rev)
+    }
 }
 
 impl Solution {
     pub fn reverse(n: i32) -> i32 {
-        let reversed = reverse_string(n.abs().to_string()).parse::<i32>().unwrap_or(0);
-        if n > 0 { reversed } else { -reversed }
+        n.checked_rev().unwrap_or(0)
     }
 }
